49a8bbcf7c8fc8d1f424761e185d16d31230161b
Artjom Juferov <artjomjuferov@Artjoms-MacBook-Pro.local>
2016-07-03 22:13:16 +0100

    Beautifier was applied

--- js/bg.js
+++ js/bg.js
@@ -1,10 +1,9 @@
-
-chrome.commands.onCommand.addListener(function (command) {
-  if (command === "search") {
-    afterSearchPressedSend("searchPressed", thenOpenGooglePage);
-  } else if (command === "close") {
-    closeLastSessionOfTabs();
-  }
+chrome.commands.onCommand.addListener(function(command) {
+    if (command === "search") {
+        afterSearchPressedSend("searchPressed", thenOpenGooglePage);
+    } else if (command === "close") {
+        closeLastSessionOfTabs();
+    }
 });
 
 
@@ -12,104 +11,116 @@ chrome.commands.onCommand.addListener(function (command) {
 // even stores search results page
 var SESSIONS = [];
 
-function closeLastSessionOfTabs(){
-  tabsToClose = SESSIONS.pop();
-  if (!tabsToClose){
-    return;
-  }
-
-  for (var i = 0; i < tabsToClose.length; i++) {
-      tabId = tabsToClose[i];
-      chrome.tabs.remove(tabId);
-  }
-}
-
-
-function afterSearchPressedSend(actionName, responseFunction){
-  chrome.tabs.query(
-    {active: true, currentWindow: true},
-    function(tabs){
-      chrome.tabs.sendMessage(
-        tabs[0].id, {action: actionName},
-        function(response) { responseFunction(response) }
-      );
+function closeLastSessionOfTabs() {
+    tabsToClose = SESSIONS.pop();
+    if (!tabsToClose) {
+        return;
     }
-  );
+
+    for (var i = 0; i < tabsToClose.length; i++) {
+        tabId = tabsToClose[i];
+        chrome.tabs.remove(tabId);
+    }
 }
 
 
-function thenOpenGooglePage(response){
-  // if it is not shown that exit from function
-  if (!response) return;
-  // var baseUrl = "https://www.google.co.uk/webhp?hl=en#newwindow=1&hl=en-GB&q=";
-  var baseUrl = "https://www.google.co.uk/search?newwindow=1&hl=en-GB&q=";
-  // var baseUrl = "https://www.google.co.uk/search?site=&source=hp&q=";
-  chrome.tabs.create({ url: baseUrl+ response.query}, thenSendMessageToParseLinks);
-}
-
-
-function thenSendMessageToParseLinks(resultsTab){
-  var sendMessageToParseLinks = function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && resultsTab.id === tabId) {
-      chrome.tabs.sendMessage(
-        resultsTab.id,
-        {action: "parseFirstLinks"},
-        function(response){
-          if (!response)
-            return;
-          // all parsed links
-          var links = response.links;
-          var tabs = [];
-          // for different purposes
-          var tabsToClose = [resultsTab.id];
-          SESSIONS.push(tabsToClose);
-          if (links.length > 0 ){
-            for (var i=0; i<links.length ; i++){
-              // open tab
-              chrome.tabs.create(
-                {url: links[i]},
-                function(activeTab){
-                  id = activeTab.id;
-                  tabs.push(id);
-                  tabsToClose.push(id);
+function afterSearchPressedSend(actionName, responseFunction) {
+    chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        },
+        function(tabs) {
+            chrome.tabs.sendMessage(
+                tabs[0].id, {
+                    action: actionName
+                },
+                function(response) {
+                    responseFunction(response)
                 }
-              );
-            }
-          }
-
-          // it is good idea to switch on first loaded tab
-          switchOnFirstLoadedTab(tabs);
-
-          // after tabs created we remove listener to prevent creating new
-          chrome.tabs.onUpdated.removeListener(sendMessageToParseLinks);
+            );
         }
-      );
+    );
+}
+
+
+function thenOpenGooglePage(response) {
+    // if it is not shown that exit from function
+    if (!response) return;
+    // var baseUrl = "https://www.google.co.uk/webhp?hl=en#newwindow=1&hl=en-GB&q=";
+    var baseUrl = "https://www.google.co.uk/search?newwindow=1&hl=en-GB&q=";
+    // var baseUrl = "https://www.google.co.uk/search?site=&source=hp&q=";
+    chrome.tabs.create({
+        url: baseUrl + response.query
+    }, thenSendMessageToParseLinks);
+}
+
+
+function thenSendMessageToParseLinks(resultsTab) {
+    var sendMessageToParseLinks = function(tabId, changeInfo, tab) {
+        if (changeInfo.status == 'complete' && resultsTab.id === tabId) {
+            chrome.tabs.sendMessage(
+                resultsTab.id, {
+                    action: "parseFirstLinks"
+                },
+                function(response) {
+                    if (!response)
+                        return;
+                    // all parsed links
+                    var links = response.links;
+                    var tabs = [];
+                    // for different purposes
+                    var tabsToClose = [resultsTab.id];
+                    SESSIONS.push(tabsToClose);
+                    if (links.length > 0) {
+                        for (var i = 0; i < links.length; i++) {
+                            // open tab
+                            chrome.tabs.create({
+                                    url: links[i]
+                                },
+                                function(activeTab) {
+                                    id = activeTab.id;
+                                    tabs.push(id);
+                                    tabsToClose.push(id);
+                                }
+                            );
+                        }
+                    }
+
+                    // it is good idea to switch on first loaded tab
+                    switchOnFirstLoadedTab(tabs);
+
+                    // after tabs created we remove listener to prevent creating new
+                    chrome.tabs.onUpdated.removeListener(sendMessageToParseLinks);
+                }
+            );
+        }
+    };
+    chrome.tabs.onUpdated.addListener(sendMessageToParseLinks)
+}
+
+
+function switchOnFirstLoadedTab(tabs) {
+    var loaded = function(tabId, changeInfo, tab) {
+        if (changeInfo.status == 'complete' && exist(tabs, tabId)) {
+            selectTab(tabId);
+            chrome.tabs.onUpdated.removeListener(loaded);
+        }
+    };
+    chrome.tabs.onUpdated.addListener(loaded)
+}
+
+
+function exist(arr, id) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == id)
+            return true;
     }
-  };
-  chrome.tabs.onUpdated.addListener(sendMessageToParseLinks)
+    return false;
 }
 
 
-function switchOnFirstLoadedTab(tabs){
-  var loaded = function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && exist(tabs, tabId)) {
-      selectTab(tabId);
-      chrome.tabs.onUpdated.removeListener(loaded);
-    }
-  };
-  chrome.tabs.onUpdated.addListener(loaded)
-}
-
-
-function exist(arr, id){
-  for(var i=0; i<arr.length; i++){
-    if (arr[i] == id)
-      return true;
-  }
-  return false;
-}
-
-
-function selectTab(tabId){
-  chrome.tabs.update(tabId, {selected: true});
+function selectTab(tabId) {
+    chrome.tabs.update(tabId, {
+        selected: true
+    });
 }
--- js/content.js
+++ js/content.js
@@ -1,133 +1,137 @@
 var LINK_TO_OPEN = 5;
 
-chrome.extension.onMessage.addListener(function(message, sender, _sendResponse){
-  var isAlreadyOpened = alreadyOpened(message.action);
-  if(message.action == "searchPressed" && isAlreadyOpened){
-    deleteAll();
-  }else if(message.action == "searchPressed" && !isAlreadyOpened){
-    addAll(_sendResponse);
-  }else if (message.action == "parseFirstLinks"){
-    startCheckingTabs(_sendResponse, LINK_TO_OPEN);
-  }else{
-    return false;
-  }
-  return true;
+chrome.extension.onMessage.addListener(function(message, sender, _sendResponse) {
+    var isAlreadyOpened = alreadyOpened(message.action);
+    if (message.action == "searchPressed" && isAlreadyOpened) {
+        deleteAll();
+    } else if (message.action == "searchPressed" && !isAlreadyOpened) {
+        addAll(_sendResponse);
+    } else if (message.action == "parseFirstLinks") {
+        startCheckingTabs(_sendResponse, LINK_TO_OPEN);
+    } else {
+        return false;
+    }
+    return true;
 });
 
 
-function alreadyOpened(actionName){
-  return actionName == "searchPressed" ? ($('#open_first_tabs_extension').length > 0) : false
+function alreadyOpened(actionName) {
+    return actionName == "searchPressed" ? ($('#open_first_tabs_extension').length > 0) : false
 }
 
 
-function startCheckingTabs(sendResponse, tabsAmount){
-  var checkExist = setInterval(function() {
-    if (appearedEnoughTabs(tabsAmount)) {
-      clearInterval(checkExist);
-      sendResponse({ links: parseFirstLinks(tabsAmount) });
-    }
-  }, 300);
+function startCheckingTabs(sendResponse, tabsAmount) {
+    var checkExist = setInterval(function() {
+        if (appearedEnoughTabs(tabsAmount)) {
+            clearInterval(checkExist);
+            sendResponse({
+                links: parseFirstLinks(tabsAmount)
+            });
+        }
+    }, 300);
 }
 
 
-function appearedEnoughTabs(tabsCount){
-  return ($('.g .r a').length > tabsCount);
+function appearedEnoughTabs(tabsCount) {
+    return ($('.g .r a').length > tabsCount);
 }
 
 
-function addAll(sendResponse){
-  AddInput(sendResponse);
-  addBgShadow();
+function addAll(sendResponse) {
+    AddInput(sendResponse);
+    addBgShadow();
 }
 
 
-function deleteAll(){
-  deleteInput();
-  deleteBgShadow();
+function deleteAll() {
+    deleteInput();
+    deleteBgShadow();
 }
 
 
 function AddInput(sendResponse) {
-  element = $('<input/>', {
-    id: 'open_first_tabs_extension',
-    placeholder: 'Enter google query'
-  }).css({
-    left: "30%",
-    top: "40%",
-    position: 'fixed',
-    width: "600px",
-    height: "60px",
-    fontSize: 24,
-    zIndex: 1024
-  }).bind("enterKey", function (e) {
-    text = $(this).val();
-    deleteAll();
-    sendResponse({query: text });
-  }).keyup(function (e) {
-    if (e.keyCode == 13)
-      $(this).trigger("enterKey");
-  }).appendTo('body');
-  element.focus();
+    element = $('<input/>', {
+        id: 'open_first_tabs_extension',
+        placeholder: 'Enter google query'
+    }).css({
+        left: "30%",
+        top: "40%",
+        position: 'fixed',
+        width: "600px",
+        height: "60px",
+        fontSize: 24,
+        zIndex: 1024
+    }).bind("enterKey", function(e) {
+        text = $(this).val();
+        deleteAll();
+        sendResponse({
+            query: text
+        });
+    }).keyup(function(e) {
+        if (e.keyCode == 13)
+            $(this).trigger("enterKey");
+    }).appendTo('body');
+    element.focus();
 }
 
 
-function deleteInput(){
-  $('#open_first_tabs_extension').remove();
+function deleteInput() {
+    $('#open_first_tabs_extension').remove();
 }
 
 
-function addBgShadow(){
-  $('<div/>', {
-    id: 'open_first_tabs_extension_bg_shadow'
-  }).css({
-    left: "0",
-    top: "0",
-    position: 'fixed',
-    width: "100%",
-    height: "100%",
-    backgroundColor: 'black',
-    opacity: 0.7,
-    zIndex: 1023
-  }).appendTo('body');
+function addBgShadow() {
+    $('<div/>', {
+        id: 'open_first_tabs_extension_bg_shadow'
+    }).css({
+        left: "0",
+        top: "0",
+        position: 'fixed',
+        width: "100%",
+        height: "100%",
+        backgroundColor: 'black',
+        opacity: 0.7,
+        zIndex: 1023
+    }).appendTo('body');
 }
 
 
-function deleteBgShadow(){
-  $('#open_first_tabs_extension_bg_shadow').remove();
+function deleteBgShadow() {
+    $('#open_first_tabs_extension_bg_shadow').remove();
 }
 
 
-function parseFirstLinks(n){
-  elements = $('.g .r a');
+function parseFirstLinks(n) {
+    elements = $('.g .r a');
 
-  if (elements.length < n)
-    n = elements.length;
+    if (elements.length < n)
+        n = elements.length;
 
-  links = [];
+    links = [];
 
-  var i = 0;
+    var i = 0;
 
-  elements.each(function(){
-    if (i>=n) return;
-    i++;
+    elements.each(function() {
+        if (i >= n) return;
+        i++;
 
-    pure_link = $(this).attr('href').split('url=');
+        pure_link = $(this).attr('href').split('url=');
 
-    if(isNotCorrect(pure_link)) return;
+        if (isNotCorrect(pure_link)) return;
 
-    pure_link =  pure_link.pop();
-    if(isNotCorrect(pure_link)) return;
+        pure_link = pure_link.pop();
+        if (isNotCorrect(pure_link)) return;
 
-    pure_link =  pure_link.split('&amp;')[0];
-    if(isNotCorrect(pure_link)) return;
+        pure_link = pure_link.split('&amp;')[0];
+        if (isNotCorrect(pure_link)) return;
 
-    links.push(pure_link);
-  });
+        links.push(pure_link);
+    });
 
-  return links;
+    return links;
 }
 
 
-function isNotCorrect(obj){
-  return obj == undefined || obj == "undefined" || obj == null;
+function isNotCorrect(obj) {
+    return obj == undefined || obj == "undefined" || obj == null;
 }
